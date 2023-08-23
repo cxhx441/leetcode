7b5be08731b33885fad45c93f7187be21c0ef1d9
Craig H <70126287+cxhx441@users.noreply.github.com>
2023-08-23 16:16:07 -0700

    start

--- str_prefix_suffix_len_calc.py
+++ str_prefix_suffix_len_calc.py
@@ -6,38 +6,40 @@ https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
 """
 import unittest
 
+
 def pattern_len(pattern: str) -> str:
     left = 0
     right = 1
-    lengths = [0]*len(pattern)
+    lengths = [0] * len(pattern)
 
     while right < len(pattern):
         if pattern[left] == pattern[right]:
-            lengths[right] = lengths[right-1] + 1
+            lengths[right] = lengths[right - 1] + 1
             left += 1
         else:
             lengths[right] = 0
             left = 0
         right += 1
 
-    return ''.join([str(x) for x in lengths])
+    return "".join([str(x) for x in lengths])
 
 
 class testPatternLen(unittest.TestCase):
     def test1(self):
-        self.assertEqual(pattern_len("abcdabeabf"),"0000120120")
+        self.assertEqual(pattern_len("abcdabeabf"), "0000120120")
 
     def test2(self):
-        self.assertEqual(pattern_len( "abcdeabfabc"), "00000120123")
+        self.assertEqual(pattern_len("abcdeabfabc"), "00000120123")
 
     def test3(self):
-        self.assertEqual(pattern_len( "aabcadaabe"), "0100101230")
+        self.assertEqual(pattern_len("aabcadaabe"), "0100101230")
 
     def test4(self):
-        self.assertEqual(pattern_len( "aaaabaacd"), "012301200")
+        self.assertEqual(pattern_len("aaaabaacd"), "012301200")
 
     def test5(self):
-        self.assertEqual(pattern_len( "abcxxxabcy"), "0000001230")
+        self.assertEqual(pattern_len("abcxxxabcy"), "0000001230")
+
 
 if __name__ == "__main__":
     unittest.main()
